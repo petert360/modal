02ef20881c42fc9957dd037b85a03898505cd66f
petert360 <peter.t360@hotmail.com>
2020-11-20 21:06:41 +0100

    Modal megjelenítés gombnyomásra

--- js/script.js
+++ js/script.js
@@ -2,13 +2,19 @@
 // Az oldal alaphelyzetben egyetlen gombot tartalmaz, "Modal felirattal."
 // Első lépés, hogy elhelyezek rajta egy eventListener-t, ami a click eseményt figyeli.
 
-let btnElements = document.querySelectorAll('.modal__open');
+let btnElements = document.querySelectorAll('.modal__openButton');
 for (let i = 0; i < btnElements.length; i++) {
     // egy anonim fv-t hívok meg, akinek az event objektumot adom át 
+    
+    //Létrehoztam két osztályt a láthatóság módosítására:
+    // a .modal--hide display:none-t állít be, a .modal--show display: flex-et az adott elementen. Mivel az egész modalt akarom megjeleníteni-eltűntetni, ezért a modal__background osztályú div-en fogom alkalmani.
+    // A második lépés, hogy a click eseményre a modal--show osztály kerül a modal__background mellé, így jelenik meg a modal maga.
+    // erre a https://www.kirupa.com/html5/setting_css_styles_using_javascript.htm oldalon találtam megoldást:
+
     btnElements[i].addEventListener('click', function (event) {
-        console.log(event.target.textContent);
+        let divElement = document.querySelector('.modal__background');
+        console.log(divElement.className);
+        divElement.className='modal__background modal--show'
+        console.log(divElement.className);
     })
 }
-
-// Eddig a click eseményre csak egy consol log jelenik meg.
-// A második lépés, hogy a click eseményre egy css tulajdonság változzon meg.
