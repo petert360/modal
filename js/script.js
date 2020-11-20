c41007d092f9f352de12b6b11c9108b236149c93
petert360 <peter.t360@hotmail.com>
2020-11-20 23:31:54 +0100

    Háttérre kattintás megoldása

--- js/script.js
+++ js/script.js
@@ -21,7 +21,21 @@ btnElement.addEventListener('click', function (event) {
 // követkző lépés, hogy ha a háttérre kattintok, akkor a modal elhalványuljon.
 // első próbálkozás, hogy a .modal__background osztályú div-re click eseményfigyelést teszek.
 // a probléma, hogy a .modal__background-ot bárhol kattintva elérem, így ez nem lesz jó megoldás.
-let backgroundElement = document.querySelector('.modal__background');
+/*let backgroundElement = document.querySelector('.modal__background');
 backgroundElement.addEventListener('click', function (event) {
     console.log('click');
+})*/
+//Új próbálkozás: windowOnClick - nem jó
+/*let modal = document.querySelector('.modal__background');
+function windowOnClick(event) {
+    if (event.target === modal) {
+        console.log('hattér');
+    }
+}*/
+// uj próba: https://webdesign.tutsplus.com/tutorials/how-to-build-flexible-modal-dialogs-with-html-css-and-javascript--cms-33500
+// ez a megoldás működik
+document.addEventListener('click', e => {
+    if (e.target == document.querySelector('.modal__background')) {
+        console.log('háttér');
+    }
 })
